6091a3aba811ea56edbaa2486720847f47722b91
Artem Lozovskiy <a.lozovskiy@tg.dunice.net>
2024-08-27 17:56:13 +0300

    fix tabs

--- algorithms/BubbleSort.ts
+++ algorithms/BubbleSort.ts
@@ -1,17 +1,17 @@
 const bubbleSort = (array: number[]): number[]  => {
-    let isSwapped;
-  
-    do {
-        isSwapped = false;
-      console.log(array);
-      array.forEach((item, index) => {
-        if (item > array[index + 1]) {
-          let tmp = item;
-          array[index] = array[index + 1];
-          array[index + 1] = tmp;
-          isSwapped = true;
-        }
-      })
-    } while (isSwapped);
-    return array
-  }
+  let isSwapped;
+
+  do {
+    isSwapped = false;
+    console.log(array);
+    array.forEach((item, index) => {
+      if (item > array[index + 1]) {
+        let tmp = item;
+        array[index] = array[index + 1];
+        array[index + 1] = tmp;
+        isSwapped = true;
+      }
+    })
+  } while (isSwapped);
+  return array
+}
--- structures/LinkedList.ts
+++ structures/LinkedList.ts
@@ -1,86 +1,86 @@
 class Node<T> {
-	value: T;
-	next: Node<T> | null;
+  value: T;
+  next: Node<T> | null;
 
-    constructor(value: T) {
-        this.value = value;
-        this.next = null;
-    }
+  constructor(value: T) {
+    this.value = value;
+    this.next = null;
+  }
 }
 class LinkedList<T> {
-	root: Node<T> | null;
+  root: Node<T> | null;
   size: number;
-	constructor() {
-			this.size = 0;
-			this.root = null;
-	}
+  constructor() {
+    this.size = 0;
+    this.root = null;
+  }
 
-	add(value: T): number {
-			if (this.size === 0) {
-					this.root = new Node(value);
-			} else {
-					let node = this.root;
-					while (node && node.next) {
-							node = node.next;
-					}
-					if (node) {
-            let newNode = new Node(value);
-            node.next = newNode;
-        }
-			}
-			this.size += 1;
-			return this.size; 
-	}
+  add(value: T): number {
+    if (this.size === 0) {
+      this.root = new Node(value);
+    } else {
+      let node = this.root;
+      while (node && node.next) {
+        node = node.next;
+      }
+      if (node) {
+        let newNode = new Node(value);
+        node.next = newNode;
+      }
+    }
+    this.size += 1;
+    return this.size; 
+  }
 
-	remove(value: T): T | null {
+  remove(value: T): T | null {
     if (this.root === null) return null;
 
     if (this.root.value === value) {
-        const removedValue = this.root.value;
-        this.root = this.root.next;
-        this.size--;
-        return removedValue;
+      const removedValue = this.root.value;
+      this.root = this.root.next;
+      this.size--;
+      return removedValue;
     }
 
     let currentNode = this.root;
 
     while (currentNode.next !== null) {
-        if (currentNode.next.value === value) {
-            const removedValue = currentNode.next.value;
-            currentNode.next = currentNode.next.next;
-            this.size--;
-            return removedValue;
-        }
-        currentNode = currentNode.next;
+      if (currentNode.next.value === value) {
+        const removedValue = currentNode.next.value;
+        currentNode.next = currentNode.next.next;
+        this.size--;
+        return removedValue;
+      }
+      currentNode = currentNode.next;
     }
 
     return null;
-	}
+  }
 
-	find(callback: (value: T) => boolean): Node<T> | null {
-			let currentNode = this.root
-			while(currentNode) {
-					if(callback(currentNode.value)) return currentNode
-					currentNode = currentNode.next
-			}
-			return null
-	}
+  find(callback: (value: T) => boolean): Node<T> | null {
+    let currentNode = this.root
+    while(currentNode) {
+      if(callback(currentNode.value)) return currentNode
+      currentNode = currentNode.next
+    }
+    return null
+  }
 
-	getSize(): number {
-			return this.size
-	}
+  getSize(): number {
+    return this.size
+  }
 
-	print(): void {
-			let result: T[] = [];
-			let node = this.root
-			while (node) {
-					result.push(node.value)
-					node = node.next
-			}
-			console.log(result);;
-	}
+  print(): void {
+    let result: T[] = [];
+    let node = this.root
+    while (node) {
+      result.push(node.value)
+      node = node.next
+    }
+    console.log(result);;
+  }
 
-	toArray(): Node<T>[] {
+  toArray(): Node<T>[] {
     const nodes: Node<T>[] = [];
     let currentNode = this.root;
 
@@ -93,4 +93,4 @@ class LinkedList<T> {
   }
 }
 
-export default LinkedList
+  export default LinkedList
